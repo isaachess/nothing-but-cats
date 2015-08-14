546fcc2227283d50dca5452a1abb472a6d342ebb
Isaac Hess <isaachess@gmail.com>
2015-08-13 21:52:19 -0600

    Force add lodash

--- src/kitteh-tv.ts
+++ src/kitteh-tv.ts
@@ -1,4 +1,5 @@
 import {formDirectives, Component, View, bootstrap} from "angular2/angular2";
+import {Http} from "angular2/http";
 
 @Component({
     selector: "kitteh-tv"
@@ -9,4 +10,24 @@ import {formDirectives, Component, View, bootstrap} from "angular2/angular2";
         <img class="stretch-width" src="/img/TV-HOF.png"/>
     `
 })
-export default class KittehTV {}
+export default class KittehTV {
+
+    constructor(
+        private http:Http
+    ) {
+        console.log('http', http.get);
+        this.grabKitteh()
+    }
+
+    grabKitteh() {
+        return this.http.get('http://www.reddit.com/r/catgifs/new.json?sort=random').toRx().toPromise()
+        .then((rs) => this.selectKitteh(rs.json().data.children))
+    }
+
+    selectKitteh(posts) {
+        var index = _.random(0, posts.length)
+        this.post = posts[index]
+        console.log('post', this.post)
+    }
+
+}
