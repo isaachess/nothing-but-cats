35d0bc7545e6aa06fc4084170debbc33ed018216
Isaac Hess <isaachess@gmail.com>
2015-08-13 22:16:37 -0600

    Basic channel changing

--- src/kitteh-tv.ts
+++ src/kitteh-tv.ts
@@ -13,22 +13,27 @@ export default class KittehTV {
     constructor(
         private http:Http
     ) {
-        console.log('http', http.get);
         this.changeChannel()
     }
 
     changeChannel() {
-        console.log('changeChannel')
         return this.http.get('http://www.reddit.com/r/catgifs/new.json?sort=random').toRx().toPromise()
         .then((rs) => this.selectKitteh(rs.json().data.children))
     }
 
     selectKitteh(posts) {
-        console.log('selectKitteh')
-        var index = _.random(0, posts.length)
-        //var filtered = _.filter(posts, (post) => post.data.url)
-        this.post = posts[index]
+        //var test1 = 'http://ma-gh.com/wp-content/uploads/cats-cute-cat-animal-cute-grass-photo.jpg'
+        //var test2 = 'https://www.google.com/images/srpr/logo11w.png'
+        //if (this.kittehUrl == test1) this.kittehUrl = test2
+        //else this.kittehUrl = test1
+        var filtered = _.filter(posts, (post) => goodUrl(post.data.url))
+        var index = _.random(0, filtered.length)
+        this.post = filtered[index]
         this.kittehUrl = this.post.data.url
         console.log('this.kittehUrl', this.kittehUrl)
     }
+}
+
+function goodUrl(url:string):boolean {
+    return _.last(url.split('.')) == 'gif'
 }
